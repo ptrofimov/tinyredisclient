d9b1435d253e3ce31f78c80726aff7bb3b5f0397
Petr Trofimov <pt@onliner.by>
2013-04-29 15:10:02 +0300

    added lazy getSocket

--- src/TinyRedisClient.php
+++ src/TinyRedisClient.php
@@ -14,20 +14,14 @@
  */
 class TinyRedisClient
 {
+    /** @var string */
+    private $server;
     /** @var resource */
-    private $_socket;
+    private $socket;
 
-    /**
-     * @param string $server e.g. "localhost:6379"
-     */
-    public function __construct($server)
+    public function __construct($server = 'localhost:6379')
     {
-        $this->_socket = stream_socket_client($server);
-    }
-
-    private function getSocket()
-    {
-        return
+        $this->server = $server;
     }
 
     public function __call($method, array $args)
@@ -37,14 +31,21 @@ class TinyRedisClient
         foreach ($args as $item) {
             $cmd .= '$' . strlen($item) . "\r\n" . $item . "\r\n";
         }
-        fwrite($this->_socket, $cmd);
+        fwrite($this->getSocket(), $cmd);
 
-        return $this->_parseResponse();
+        return $this->parseResponse();
     }
 
-    private function _parseResponse()
+    private function getSocket()
     {
-        $line = fgets($this->_socket);
+        return $this->socket
+            ? $this->socket
+            : ($this->socket = stream_socket_client($this->server));
+    }
+
+    private function parseResponse()
+    {
+        $line = fgets($this->getSocket());
         list($type, $result) = array($line[0], substr($line, 1, strlen($line) - 3));
         if ($type == '-') { // error message
             throw new Exception($result);
@@ -52,13 +53,13 @@ class TinyRedisClient
             if ($result == -1) {
                 $result = null;
             } else {
-                $line = fread($this->_socket, $result + 2);
+                $line = fread($this->getSocket(), $result + 2);
                 $result = substr($line, 0, strlen($line) - 2);
             }
         } elseif ($type == '*') { // multi-bulk reply
             $count = ( int ) $result;
             for ($i = 0, $result = array(); $i < $count; $i++) {
-                $result[] = $this->_parseResponse();
+                $result[] = $this->parseResponse();
             }
         }
 
